9bdd49ff31d5bcbcf1efb709656df1b8d6822638
Aleksandra Ilic <95101761+AleksandraNorway@users.noreply.github.com>
2024-02-12 13:26:33 +0100

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="./troll-face.png" 
+                alt="Troll face" // Add alt attribute here
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Project 3</h4>
+        </header>
+    )
+}
